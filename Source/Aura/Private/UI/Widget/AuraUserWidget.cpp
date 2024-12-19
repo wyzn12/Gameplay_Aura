cdca12b6f38f2b2f3d5b98491e9c671f0817240c
wyzn12 <89501513@qq.com>
2024-12-20 04:59:09 +0800

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright wyzn12
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
