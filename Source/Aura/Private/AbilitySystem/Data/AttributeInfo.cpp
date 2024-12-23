dce42aacbb69559a64237f502e24251feabc17ca
wyzn12 <89501513@qq.com>
2024-12-23 15:16:27 +0800

    Attribute Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,25 @@
+// Copyright wyzn12
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+#include "AttributeSet.h"
+#include "Animation/AnimAttributes.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttribute(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find Info for AttributeTag [%s] on AttributeInfo [%s]"), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+	
+	return FAuraAttributeInfo();
+}
